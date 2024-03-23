79fa9a919e2f974d1447595f1dcf0fc0d73126a2
Sepideh <sepideh.badran05@gmail.com>
2024-03-23 20:14:32 +0330

    exersice10

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+
+Console.WriteLine("Hello, World!");
+class program
+{
+    static void Main(string[] args)
+    {
+for (int number = 100; number < 1000; number++) {
+            int digit1 = number / 100;
+            int digit2 = (number / 10) % 10;
+            int digit3 =number % 10;
+            int product = digit1 * digit2 * digit3;
+            if (product > number  / 2)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+    }
